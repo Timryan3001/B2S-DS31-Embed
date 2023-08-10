0b7325e98fbfacc24edae00d06645bc1d44f2a2c
Timryan3001 <timryan3001@gmail.com>
2023-08-10 13:58:26 +0100

    Adding functionality to buttons

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // console.log('Hello DS31')
 
+let viz;
+
 // 1. create a variable to store our vizcontainer
 
 const vizBox = document.getElementById("vizContainer");
@@ -19,9 +21,33 @@ const url = "https://public.tableau.com/views/EmbeddingWorkbookProfitsAcrossME-A
 // 4. define our function to build dashboard
 
 function initViz() {
-    const viz = new tableau.Viz(vizBox, url, options);
+    viz = new tableau.Viz(vizBox, url, options);
 }
 
 // 5. controlling when function is run
 
 document.addEventListener("DOMContentLoaded", initViz);
+
+// 6. adding pdf export button functionality
+
+document.addEventListener("DOMContentLoaded", function () {
+    const exportPdfButton = document.getElementById("exportPdf");
+
+    exportPdfButton.addEventListener("click", exportPdfFunction);
+
+    function exportPdfFunction() {
+        viz.showExportPDFDialog();
+    }
+});
+
+// 7. adding ppt export button functionality
+
+document.addEventListener("DOMContentLoaded", function () {
+    const exportPptButton = document.getElementById("exportPpt");
+
+    exportPptButton.addEventListener("click", exportPptFunction);
+
+    function exportPptFunction() {
+        viz.showExportPowerPointDialog();
+    }
+});
